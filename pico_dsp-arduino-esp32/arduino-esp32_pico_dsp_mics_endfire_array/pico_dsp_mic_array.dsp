f916e66ac59c603ce85d5bb0f0613575268adba2
andy-wi <andywilson@tuta.io>
2021-08-20 11:23:17 +0100

    Add endfire array example

---
+++ pico_dsp-arduino-esp32/arduino-esp32_pico_dsp_mics_endfire_array/pico_dsp_mic_array.dsp
@@ -0,0 +1,24 @@
+import("stdfaust.lib");
+// fs = 48000; // or fs = 24000;
+
+// pico dsp left(L) microphone is the one closest to the TRS connectors, right(R) microphone is the one closest to the RF antenna 
+// mic spacing is (d = n x v/fs), where n = number of samples to delay // 1 x (343/24000) = 14.29mm
+
+L = _ : fi.dcblocker;  // adjust for mic input dc offset, which is ~ 0.7v
+R = _ : fi.dcblocker; 
+
+// omnidirectional //
+//process = L, R;   // comment out this line for both mics in omnidirectional configuration 
+
+// broadside array (sound at 90 and 270 degrees off axis is attenuated) //
+// (for an array with 2 microphones where d (the distance between the microphones) is = 14.29mm and /// the velocity of sound(v) is 343 m/sec, there should be a perfect null at ~ 12kHz (0.5 x v/fs)
+// (343 m/sec ÷ (0.01429 m × 2) = 12kHz  
+//process = L, R : +; // comment out this line for broadside array configuration
+
+// differential endfire array (directional, cardiod/hypercardiod pickup pattern) // 
+// delay the signal from the back microphone by the same time that it takes the sound to travel between the two microphones, and subtract it (or invert and sum) from the front microphone signal  
+process = L @6, R: -;  // differential endfire array 
+//process = L @3 *-1, R: +;  // differential endfire array, invert and sum
+			
+
+			
---
+++ pico_dsp-arduino-esp32/arduino-esp32_pico_dsp_mics_endfire_array/src/picodsp_mics_endfire_array.h
@@ -0,0 +1,66 @@
+/************************************************************************
+ FAUST Architecture File
+ Copyright (C) 2019-2020 GRAME, Centre National de Creation Musicale &
+ Aalborg University (Copenhagen, Denmark)
+ ---------------------------------------------------------------------
+ This Architecture section is free software; you can redistribute it
+ and/or modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 3 of
+ the License, or (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; If not, see <http://www.gnu.org/licenses/>.
+ 
+ EXCEPTION : As a special exception, you may create a larger work
+ that contains this FAUST architecture section and distribute
+ that work under terms of your choice, so long as this FAUST
+ architecture section is not modified.
+ 
+ ************************************************************************/
+
+#ifndef faust_picodsp_mics_endfire_array_h_
+#define faust_picodsp_mics_endfire_array_h_
+
+#include <string>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/i2s.h"
+
+class dsp;
+class esp32audio;
+class MapUI;
+#ifdef MIDICTRL
+class MidiUI;
+class esp32_midi;
+#endif
+
+class picodsp_mics_endfire_array
+{
+    private:
+    
+        esp32audio* fAudio;
+    	dsp* fDSP;
+        MapUI* fUI;
+    #ifdef MIDICTRL
+        esp32_midi* fMIDIHandler;        
+        MidiUI* fMIDIInterface;
+    #endif
+
+    public:
+    
+        picodsp_mics_endfire_array(int sample_rate, int buffer_size);
+        ~picodsp_mics_endfire_array();
+    
+        bool start();
+        void stop();
+    
+        void setParamValue(const std::string&, float);
+        float getParamValue(const std::string& path);
+};
+
+#endif
